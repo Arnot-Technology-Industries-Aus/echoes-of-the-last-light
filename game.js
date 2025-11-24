a2aae66adaa9bc8e919305cac06740ed99d7bf33
Arnot-Technology-Industries-Aus <arnot.cooper@gmail.com>
2025-11-24 13:30:53 +1100

    Update game.js

--- game.js
+++ game.js
@@ -1,48 +1,46 @@
 // Initialize Kaboom
 kaboom({
-    width: 800,
-    height: 600,
-    scale: 2,
+    width: 640,
+    height: 480,
+    scale: 1,
     clearColor: [0, 0, 0, 1], // black background
     debug: true,
 });
 
-// Player (Kael) as a blue rectangle
+// Create Kael (player) as blue rectangle
 const kael = add([
     rect(32, 48),
-    pos(100, 300),
+    pos(100, 200),
     color(0, 0, 1), // blue
     area(),
-    body(),
 ]);
 
-// Companion (Lux) as a yellow rectangle
+// Create Lux (companion) as yellow rectangle
 const lux = add([
     rect(24, 24),
-    pos(150, 250),
+    pos(150, 150),
     color(1, 1, 0), // yellow
-    area()
+    area(),
 ]);
 
-// Lux follows Kael smoothly
+// Make Lux follow Kael smoothly
 action(lux, () => {
     lux.pos = lux.pos.lerp(kael.pos.add(vec2(50, -50)), 0.05);
 });
 
-// Player movement
+// Player movement with arrow keys
 keyDown("left", () => kael.move(-120, 0));
 keyDown("right", () => kael.move(120, 0));
 keyDown("up", () => kael.move(0, -120));
 keyDown("down", () => kael.move(0, 120));
 
-// Circular light around Lux
+// Light effect: yellow semi-transparent circle following Lux
 const light = add([
-    circle(80),             // radius
+    circle(80),
     pos(lux.pos),
-    color(1, 1, 0, 0.3),    // semi-transparent yellow
+    color(1, 1, 0, 0.3),
 ]);
 
-// Make light follow Lux
 action(light, () => {
     light.pos = lux.pos;
 });
